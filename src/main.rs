8f2de9ec251d536692ff1dae6c6a360d3cec38b1
Josep M. Bach <josep.m.bach@gmail.com>
2014-06-09 16:51:17 +0200

    Display values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate ract;
+
+use ract::value::{RInt,RList,RStr};
+
 fn main() {
-  println!("hello world");
+  let my_list = RList(vec![RInt(42), RStr("foo".to_string())]);
+  println!("{}", my_list);
 }
--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,27 @@
+use std::fmt;
+
 pub enum Value {
   RInt(int),
   RStr(String),
+  RList(Vec<Value>),
   RNil
+}
+
+impl fmt::Show for Value {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      RNil           => write!(f, "nil"),
+      RInt(i)        => write!(f, "{:i}", i),
+      RStr(ref s)    => write!(f, "\"{:s}\"", s.as_slice()),
+      RList(ref arr) => {
+        let mut string: String = "".to_string();
+        for &ref elem in arr.iter() {
+          string.push_str(format!("{}", elem).as_slice());
+          string.push_char(' ');
+        }
+        string.pop_char();
+        write!(f, "({:s})", string)
+      }
+    }
+  }
 }
